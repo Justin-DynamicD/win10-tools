81be058fb8793d351fb93829a6d8b8497a249b13
Justin King <justin.dynamicd@gmail.com>
2019-03-13 21:40:57 -0700

    save this guy

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,18 @@
+﻿function prompt {
+  $host.ui.rawui.WindowTitle = $(get-location) 
+  $prompt_string = ""
+
+  if(Test-Path .git) {
+    $prompt_string = "PS " + (pwd).Path + " (GIT"
+    git branch | foreach {
+      if ($_ -match "^\*(.*)"){
+        $prompt_string += $matches[1] + ")> "
+      }
+    }
+  }
+  else{
+    $prompt_string = "PS " + (pwd).Path + "> "
+  }   
+  Write-Host ($prompt_string) -nonewline -foregroundcolor yellow
+  return " "
+}
